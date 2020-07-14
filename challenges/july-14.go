409cb910039cbe0c520d9d186bd5486de71edd8c
Chiranthan S <chiras@cisco.com>
2020-07-14 19:14:10 +0530

    removed fmt

--- challenges/july-14.go
+++ challenges/july-14.go
@@ -1,12 +1,11 @@
 package challenges
 
 import (
-	"fmt"
 	"math"
 )
 
 // AngleClock -
 func AngleClock(hour int, minutes int) float64 {
-	angle := math.Abs(float64(float64(30*hour) + (float64(minutes) / 2)) - float64(minutes * 6))
+	angle := math.Abs(float64(float64(30*hour)+(float64(minutes)/2)) - float64(minutes*6))
 	return math.Min(angle, 360-angle)
 }
