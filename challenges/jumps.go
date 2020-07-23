e5fc571647efcb3ec7c931b4c92ab0ff099db95a
Chiranthan S <chiras@cisco.com>
2020-07-24 00:04:16 +0530

    jumps.go

---
+++ challenges/jumps.go
@@ -0,0 +1,23 @@
+package challenges
+
+func canReach(arr []int, start int) bool {
+	visited := make([]bool, len(arr))
+	return dfsJump(start, arr, visited)
+}
+
+func dfsJump(start int, arr []int, visited []bool) bool {
+	if arr[start] == 0 {
+		return true
+	}
+	var front, back bool
+	visited[start] = true
+
+	if start+arr[start] < len(arr) && !visited[start+arr[start]] {
+		front = dfsJump(start+arr[start], arr, visited)
+	}
+
+	if start-arr[start] >= 0 && !visited[start-arr[start]] {
+		back = dfsJump(start-arr[start], arr, visited)
+	}
+	return front || back
+}
